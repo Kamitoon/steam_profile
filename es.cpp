933ba4cb481911afac6ae690ec64b166d3338e28
Kamitoon <57278362+Kamitoon@users.noreply.github.com>
2020-01-22 16:10:44 -0300

    Update es.cpp

--- es.cpp
+++ es.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
-/ * No acepto un perfil privado o VAC, gracias por su comprensión;
-No me pidas juegos o artículos * /
+/* No acepto un perfil privado o VAC, gracias por su comprensión;
+No me pidas juegos o artículos */
 
 /// es
 Informaciones personales{
@@ -17,24 +17,24 @@ Informaciones personales{
 --- Lenguajes:Python, C, C ++;
 }
 
-PC {
+PC{
 --- CPU:Intel Core 2 Quad Q9300;
 --- Placa base: Gigabyte G31M-S2L;
---- RAM: 4 GB;
---- HD: 500 GB;
---- GPU: AMD Radeon HD 6570 2GB DDR3 128bits;
---- SO: Windows 10 Home 64 bits (Original);
+--- RAM:4 GB;
+--- HD:500 GB;
+--- GPU:AMD Radeon HD 6570 2GB DDR3 128bits;
+--- SO:Windows 10 Home 64 bits (Original);
 }
 
-Equipo {
---- Monitor: Samsung SyncMaster 732NW;
---- Teclado: Multilaser TC193;
---- Ratón: Redragon Nothosaur;
---- Mousepad: WinPad Blood;
---- Auriculares: Fortrek Gamer Pro H2;
+Equipo{
+--- Monitor:Samsung SyncMaster 732NW;
+--- Teclado:Multilaser TC193;
+--- Ratón:Redragon Nothosaur;
+--- Mousepad:WinPad Blood;
+--- Auriculares:Fortrek Gamer Pro H2;
 }
 
-CS:GO configuraciones en el juego {
+CS:GO configuraciones en el juego{
 --- Resolución:1280x960 (4: 3);
 --- Sensibilidad:0,70;
 --- DPI:800;
