96b5e442493dc4a539930982594180421050f275
Kamitoon <57278362+Kamitoon@users.noreply.github.com>
2020-01-21 22:03:45 -0300

    Create es.cpp

---
+++ es.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+/ * No acepto un perfil privado o VAC, gracias por su comprensión;
+No me pidas juegos o artículos * /
+
+/// es
+Informaciones personales{
+--- Nombre:Rafael;
+--- Edad:18 años de edad;
+--- Altura:1,65 m;
+--- Nacimiento:25/04/2001;
+--- Sexo:Masculino;
+--- Profesión:Estudiante;
+--- Nacionalidad:Brasileña;
+--- Estado:Rio Grande do Sul;
+--- Idiomas:Portugués, Inglés;
+--- Lenguajes:Python, C, C ++;
+}
+
+PC {
+--- CPU:Intel Core 2 Quad Q9300;
+--- Placa base: Gigabyte G31M-S2L;
+--- RAM: 4 GB;
+--- HD: 500 GB;
+--- GPU: AMD Radeon HD 6570 2GB DDR3 128bits;
+--- SO: Windows 10 Home 64 bits (Original);
+}
+
+Equipo {
+--- Monitor: Samsung SyncMaster 732NW;
+--- Teclado: Multilaser TC193;
+--- Ratón: Redragon Nothosaur;
+--- Mousepad: WinPad Blood;
+--- Auriculares: Fortrek Gamer Pro H2;
+}
+
+CS:GO configuraciones en el juego {
+--- Resolución:1280x960 (4: 3);
+--- Sensibilidad:0,70;
+--- DPI:800;
+}
+
+return 0;
