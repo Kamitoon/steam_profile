395926d52147fd81448fab05bee8a1c587e30ede
Kamitoon <57278362+Kamitoon@users.noreply.github.com>
2020-01-21 21:51:40 -0300

    Create en_us.cpp

---
+++ en_us.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+/* I don't accept a private or VAC profile. Thank you for understanding;
+Don't ask me for games or items. */
+
+Personal Information{
+--- Name:Rafael;
+--- Age:18 years;
+--- Height:1.65 m;
+--- Birth:4/25/2001;
+--- Gender:Male;
+--- Profession:Student;
+--- Nationality:Brazilian;
+--- State:Rio Grande do Sul;
+--- Languages:Portuguese, English;
+--- Languages:Python, C, C ++;
+}
+
+PC{
+--- CPU:Intel Core 2 Quad Q9300;
+--- Motherboard:Gigabyte G31M-S2L;
+--- RAM:4GB;
+--- HD:500 GB;
+--- GPU:AMD Radeon HD 6570 2GB DDR3 128bit;
+--- OS:Windows 10 Home 64-bit (Original);
+}
+
+Equipments{
+--- Monitor:Samsung SyncMaster 732NW;
+--- Keyboard:Multilaser TC193;
+--- Mouse:Redragon Nothosaur;
+--- Mousepad:WinPad Blood;
+--- Headset:Fortrek Gamer Pro H2;
+}
+
+CS:GO in-game configs{
+--- Resolution:1280x960 (4:3);
+--- Sensitivity:0.70;
+--- DPI:800;
+}
+
+return 0;
