2b2372124c70a76da8c86f9fd514f7189542d30e
Kamitoon <57278362+Kamitoon@users.noreply.github.com>
2020-01-21 21:48:20 -0300

    Create pt_br.cpp

---
+++ pt_br.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+/* Não aceito perfil privado ou com VAC, obrigado pela compreensão;
+Não me peça jogos ou itens*/
+
+///pt_br
+Informações pessoais{
+--- Nome:Rafael;
+--- Idade:18 anos;
+--- Altura:1,65 m;
+--- Nascimento:25/04/2001;
+--- Sexo:Masculino;
+--- Profissão:Estudante;
+--- Nacionalidade:Brasileiro;
+--- Estado:Rio Grande do Sul;
+--- Idiomas:Português, Inglês;
+--- Linguagens:Python, C, C++;
+}
+
+PC{
+--- CPU:Intel Core 2 Quad Q9300;
+--- Placa mãe:Gigabyte G31M-S2L;
+--- RAM:4GB;
+--- HD:500 GB;
+--- GPU:AMD Radeon HD 6570 2GB DDR3 128bits;
+--- SO:Windows 10 Home 64 bits (Original);
+}
+
+Equipamentos{
+--- Monitor:Samsung SyncMaster 732NW;
+--- Teclado:Multilaser TC193;
+--- Mouse:Redragon Nothosaur;
+--- Mousepad:WinPad Blood;
+--- Headset:Fortrek Gamer Pro H2;
+}
+
+CS:GO configs in-game{
+--- Resolução:1280x960 (4:3);
+--- Sensibilidade:0.70;
+--- DPI:800;
+}
